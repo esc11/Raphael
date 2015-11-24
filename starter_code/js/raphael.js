24dee1f8811bb51d2fd2ba208d318e0e96cba1a5
esc11 <esc11@uw.edu>
2015-11-23 21:46:41 -0800

    completed tutorial

--- starter_code/js/raphael.js
+++ starter_code/js/raphael.js
@@ -5,21 +5,21 @@ $(document).ready(function() {
     // Creates canvas 500 × 500 at 50, 50
 	var paper = new Raphael(50, 50, 500, 500);
 
-	//Creates circle at x = 50, y = 40, with radius 10
-	var circle = paper.circle(50, 40, 10);
+	// //Creates circle at x = 50, y = 40, with radius 10
+	// var circle = paper.circle(50, 40, 10);
 
-	// Sets the fill attribute of the circle to red (#f00)
-	circle.attr("fill", "#f00");
+	// // Sets the fill attribute of the circle to red (#f00)
+	// circle.attr("fill", "#f00");
 
-	// // // Sets the stroke attribute of the circle to white
-	circle.attr("stroke", "#fff");
-	circle.attr("stroke", "white");
+	// // // // Sets the stroke attribute of the circle to white
+	// circle.attr("stroke", "#fff");
+	// circle.attr("stroke", "white");
 
 	// Create pathways. M means to start drawing without a visible path at designated x and y coordinates.
 	// l means to start drawing pathways between coordinates, and z means to close the path.
-	//var tetrisPiece = paper.path("M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z");
+	var tetrisPiece = paper.path("M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z");
 	
-	/*tetrisPiece.attr(
+	tetrisPiece.attr(
         {
         	// 90 degree gradient from dark blue to light blue
             gradient: '90-#526c7a-#64a0c1',
@@ -30,24 +30,30 @@ $(document).ready(function() {
             // round the corners
             'stroke-linejoin': 'round'
         }
-    );*/
+    );
 
     //tetrisPiece.rotate(-90);
 
-    //bounce easing formula, the default is linear
+    // bounce easing formula, the default is linear
     // tetrisPiece.animate({ stroke: '#3b4449'}, 1000, 'bounce', function() {
     // 	this.rotate(-90);
     // });
 
-    // tetrisPiece.animate({stroke: '#3b4449'}, 1000, 'bounce', function() {
-    // 	this.animate({
-	   //      stroke: 'purple'
-	   //  }).rotate(-90);
-    // });
+    tetrisPiece.animate({stroke: '#3b4449'}, 1000, 'bounce', function() {
+    	this.animate({
+	        stroke: 'purple'
+	    }).rotate(-90);
+    });
 
 	//** Shift tetris object**//
 
+    $(document).keydown(function(event) {
+        if (event.which == 37) {
+            tetrisPiece.animate({transform: "t-10,0"},'linear', 2000)
+        }
+    })
+
     //**spin demo**//
-
-
+    var spin = Raphael.animation({transform: "...r360"}, 4000).repeat(Infinity);
+    tetrisPiece.animate(spin, 'linear');
 });
